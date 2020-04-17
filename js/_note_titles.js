8d115ac48fd4ba0f66c0fb3c7d4b23f7bec08a99
M Srisaravana <saravanamuthaly@gmail.com>
2020-04-17 19:32:42 +0530

    v 0.10 - window title updates

---
+++ js/_note_titles.js
@@ -0,0 +1,7 @@
+const noteTitles = [
+    "there is a voice that doesn’t use words",
+    "be like a tree and let the dead leaves drop",
+    "it is your light that lights the worlds",
+    "when will you begin that long journey into yourself?"
+];
+
